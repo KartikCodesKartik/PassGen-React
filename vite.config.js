4021afca283d013633e7a8b96addb21ff177812e
KartikCodesKartik <me.cldkartik@gmail.com>
2024-07-19 15:18:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Todo-React/', // Base path for GitHub Pages deployment
+  base: '/PassGen-React/', // Base path for GitHub Pages deployment
   plugins: [react()],
 });
