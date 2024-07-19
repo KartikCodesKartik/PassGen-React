dece42afb8b6e9d190fc6ce4d46098eb016f5193
Kartikey Sharma <memodinarendar@gmail.com>
2024-07-19 15:04:16 +0530

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-//export default defineConfig({
- // plugins: [react()],
-//})
-
-export default defineConfig({ base: "/{Todo-React}", plugins: [react()], }) 
+export default defineConfig({
+  base: '/Todo-React/', // Base path for GitHub Pages deployment
+  plugins: [react()],
+});
